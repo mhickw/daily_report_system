af289e2e9d203e520fd3b4d7e3c98678dd7accf7
mi.isaka <mintcat19@hotmail.co.jp>
2018-07-26 23:08:55 +0900

    Modify top page

--- src/models/Report.java
+++ src/models/Report.java
@@ -17,95 +17,92 @@ import javax.persistence.Table;
 
 @Table(name = "reports")
 @NamedQueries({
-    @NamedQuery(
-            name = "getAllReports",
-            query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
-            ),
-    @NamedQuery(
-            name = "getReportsCount",
-            query = "SELECT COUNT(r) FROM Report AS r"
-            ),
+        @NamedQuery(name = "getAllReports", query = "SELECT r FROM Report AS r ORDER BY r.id DESC"),
+        @NamedQuery(name = "getReportsCount", query = "SELECT COUNT(r) FROM Report AS r"),
+        @NamedQuery(name = "getMyAllReports", query = "SELECT r FROM Report AS r WHERE r.employee = :employee ORDER BY r.id DESC"),
+
+        @NamedQuery(name = "getMyReportsCount", query = "SELECT COUNT(r) FROM Report AS r WHERE r.employee = :employee")
 })
 @Entity
 public class Report {
-@Id
-@Column(name = "id")
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Integer id;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-@ManyToOne
-@JoinColumn(name = "employee_id", nullable = false)
-private Employee employee;
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
 
-@Column(name = "report_date" , nullable =false)
-private Date report_date;
+    @Column(name = "report_date", nullable = false)
+    private Date report_date;
 
-@Column(name = "title",length = 255,nullable = false)
-private String title;
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
 
-@Lob
-@Column(name = "content", nullable = false)
-private String content;
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
 
-@Column(name = "created_at", nullable = false)
-private Timestamp created_at;
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
 
-@Column(name ="updated_at", nullable = false)
-private Timestamp updated_at;
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
 
-public Integer getId(){
-    return id;
-}
-
-public void setId(Integer id){
-    this.id = id;
-}
-
-public Employee getEmployee(){
-    return employee;
-   }
-
-public void setEmployee(Employee employee){
-    this.employee = employee;
-}
-
-public Date getReport_date(){
-    return report_date;
-}
-
-public void setReport_date(Date report_date){
-    this.report_date = report_date;
+    public Integer getId() {
+        return id;
     }
 
-public String getTitle(){
-    return title;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-public void setTitle(String title){
-    this.title = title;
-}
+    public Employee getEmployee() {
+        return employee;
+    }
 
-public String getContent(){
-    return content;
-}
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
 
-public void setContent(String content){
-this.content = content;
-}
+    public Date getReport_date() {
+        return report_date;
+    }
 
-public Timestamp getCreated_at(){
-    return created_at;
-}
+    public void setReport_date(Date report_date) {
+        this.report_date = report_date;
+    }
 
-public void setCreated_at(Timestamp created_at){
-this.created_at = created_at;
-}
+    public String getTitle() {
+        return title;
+    }
 
-public Timestamp getUpdated_at(){
-    return updated_at;
-}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-public void setUpdated_at(Timestamp updated_at){
-    this.updated_at = updated_at;
-}
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
 }
